e62c45450adb0bb7fe12beb4208c33131dc703e8
Erdem Turk <erdemturk48@gmail.com>
2023-11-30 15:14:30 +0300

    Update depthCalculation.py

--- scripts/depthCalculation.py
+++ scripts/depthCalculation.py
@@ -4,26 +4,24 @@ from re import sub
 import sys
 import shlex
 import subprocess
-
+import configPath
 
 
 def filterBedFile(inFile,threshold,outFile):
-    filterCMD = "awk -v threshold=" + threshold + " '$4<=threshold' " + inFile
-    # " > " + os.path.splitext(f)[0].split(".")[0] + ".per-base_filtered.bed"
-    filterArgs = shlex.split(filterCMD)
-    filteredBed = subprocess.run(filterArgs, capture_output=True, check=False, text=True, encoding='utf-8').stdout
+    with open(infile, "r") as input_file:
+        lines = input_file.readlines()
+    filtered_lines = [line for line in lines if float(line.split()[2]) <= float(threshold)]
     out = open(outFile,"w")
     out.write(filteredBed)
+    for line in filtered_lines:
+        cols = line.strip().split("\t")
+        output_file.write(cols[0] + "\t" + cols[1] + "\t" + cols[1] + "\t" + cols[2] + "\n")
     out.close()
     subprocess.run(["rm", inFile])
     subprocess.run(["mv", outFile, inFile])
 
-
-
 workingDir = sys.argv[1]
 sys.path.append(workingDir + "/" + ".config" + "/")
-import configPath
-
 
 capture = os.path.join(workingDir + "/userUpload/captureFile/")
 for i in os.listdir(capture):
@@ -33,8 +31,6 @@ depth_threshold = sys.argv[2]
 
 alignmentDir = os.path.join(workingDir + "/userUpload/samFile/")
 
-
-
 out_dir =  os.path.join(workingDir + "/userUpload/mosdepth/")
 
 def deleteFile():
@@ -43,17 +39,15 @@ def deleteFile():
         if(not(f.endswith(".per-base.bed.gz") or f.endswith(".per-base.bed.gz.tbi"))):
             os.remove(f)
 
-
 for filename in os.listdir(alignmentDir):
     f = os.path.join(alignmentDir, filename)
     if os.path.isfile(f) and f.endswith("_sorted.bam"):
         file = filename.split("_")[0]
-        mosdepthCmd = (configPath.mosdepth + " -t 4 --by " + capturefile + " " + file + " " + f)
-        print(mosdepthCmd)
-        mosdepthArgs = shlex.split(mosdepthCmd)
-        subprocess.run(mosdepthArgs, cwd=out_dir)
-
-
+        samtoolsCmd = (samtoolsPath + " depth -b " + capture + " " + f + " -o " + file + ".per-base.bed")
+        samtoolsArgs = shlex.split(samtoolsCmd)
+        subprocess.run(samtoolsArgs, cwd=out_dir)
+        subprocess.run(shlex.split("gzip " + out_dir + "/" + file + ".per-base.bed"))
+        
 for filename in os.listdir(out_dir):
     f = os.path.join(out_dir,filename)
     if os.path.isfile(f) and ("per-base" in filename) and f.endswith(".gz"):
@@ -61,16 +55,12 @@ for filename in os.listdir(out_dir):
         gzipArg = shlex.split(gzipCMD)
         subprocess.run(gzipArg)
     
-
         filterBedFile(os.path.splitext(f)[0],depth_threshold,os.path.splitext(f)[0].split(".")[0] + ".per-base_filtered.bed")
         
-       
-
         bgzipCmd = ("bgzip" + " -@ " + str(os.cpu_count()) + " " + os.path.splitext(f)[0])
         bgzipArgs = shlex.split(bgzipCmd)
         subprocess.run(bgzipArgs, cwd=out_dir)
 
-       
         tabixCmd = ("tabix" + " -p bed " + f)
         tabixArgs = shlex.split(tabixCmd)
         subprocess.run(tabixArgs, cwd=out_dir)
