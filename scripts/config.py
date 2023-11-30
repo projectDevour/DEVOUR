de7df0c582dc0656a2c757de3ebc447ec343b6f8
Erdem Turk <erdemturk48@gmail.com>
2023-11-30 15:26:27 +0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -7,12 +7,13 @@ workingDir = sys.argv[1]
 
 paths = sys.argv[2]
 paths2= paths.split(",")
-print(paths2)
+# print(paths2)
 
 f = os.path.join(workingDir + ".config/", "configPath.py")
 
 if(not os.path.isfile(f)):
     fp = open(f, 'w')
-    fp.write("mosdepth = " + "'" + paths2[0] + "'" + "\n" + "samtools = " + "'" + paths2[1] + "'" + "\n" + "annovar = " + "'" +paths2[2]+ "'" )
+    # fp.write("mosdepth = " + "'" + paths2[0] + "'" + "\n" + "samtools = " + "'" + paths2[1] + "'" + "\n" + "annovar = " + "'" +paths2[2]+ "'" )
+    fp.write("samtools = " + "'" + paths2[0] + "'" + "\n" + "annovar = " + "'" +paths2[1]+ "'" )
     fp.close()
 
